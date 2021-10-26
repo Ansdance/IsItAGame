fce4554221b74400bfecf44f445fc29ebcf0c7ed
ansar_office <adaulebayev@magnumtech.kz>
2021-10-26 10:24:52 +0600

    first commit

---
+++ src/test/java/com/example/MyFirstGame/MyFirstGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyFirstGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
